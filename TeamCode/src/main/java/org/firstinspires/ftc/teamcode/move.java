ee4ec16d7e4bd753b57c78d905532bedbc164ed6
Skyfather1 <56570077+Skyfather1@users.noreply.github.com>
2019-11-23 16:04:11 -0700

    Dont work now will try to get them to work this week... -Ethan

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/move.java
@@ -0,0 +1,26 @@
+/*package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+public class move extends LinearOpMode {
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+    public move(){
+
+        //Ni-chan! Dont forget hardware.
+        motorFL = hardwareMap.dcMotor.get("motorDriveFrontLeft");
+        motorFR = hardwareMap.dcMotor.get("motorDriveFrontRight");
+        motorBL = hardwareMap.dcMotor.get("motorDriveBackLeft");
+        motorBR = hardwareMap.dcMotor.get("motorDriveBackRight");
+
+        //Wow much motor going ways.
+        motorDriveBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorDriveBackRight.setDirection(DcMotor.Direction.FORWARD);
+        motorDriveFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorDriveFrontRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+}*/
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/one.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/one.java
@@ -6,45 +6,29 @@ package org.firstinspires.ftc.teamcode;
  * Goal: Control space and time.
  */
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
+ /*import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+ import com.qualcomm.robotcore.hardware.DcMotor;
+ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "One (thing to rule them all)", group = "")
 
 //Do you even java.
 public class one extends LinearOpMode {
-    // Create motor thing well do real hardware later ↓
-    private DcMotor motorDriveBackLeft;
-    private DcMotor motorDriveBackRight;
-    private DcMotor motorDriveFrontLeft;
-    private DcMotor motorDriveFrontRight;
-
     //OwO whats this??
     @Override
     public void runOpMode() {
-        //Reference hardware configuration variables
-        motorDriveBackLeft = hardwareMap.dcMotor.get("motorDriveBackLeft");
-        motorDriveBackRight = hardwareMap.dcMotor.get("motorDriveBackRight");
-        motorDriveFrontLeft = hardwareMap.dcMotor.get("motorDriveFrontLeft");
-        motorDriveFrontRight = hardwareMap.dcMotor.get("motorDriveFrontRight");
-
-        //Wow much motor going ways.
-        motorDriveBackLeft.setDirection(DcMotor.Direction.REVERSE);
-        motorDriveBackRight.setDirection(DcMotor.Direction.FORWARD);
-        motorDriveFrontLeft.setDirection(DcMotor.Direction.FORWARD);
-        motorDriveFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        move motors = new move();
 
         //Console.log for java ftc bull****
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // OwO Big red buttion press.
+        // OwO Await big red buttion press.
         waitForStart();
-        // Wow runner cool funni
         while (opModeIsActive()) {
-            //Code.java
+
         }
     }
 }
+*/
