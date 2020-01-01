476f59ffb6369f7ded9fc8a1b43ad91daa2ec6f2
carterjandrew <52466114+carterjandrew@users.noreply.github.com>
2020-01-01 16:10:43 -0700

    Didnt commit all varables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
@@ -2,6 +2,6 @@ public class RobotVarables {
     public RobotVarables() {
         int elevatorMax = 0; //<- elevator max height
         double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
-        double trayServo[] = {};
+        double trayServo[] = {0.3, 0.8}; //clasp down, clasp up
     }
 }
