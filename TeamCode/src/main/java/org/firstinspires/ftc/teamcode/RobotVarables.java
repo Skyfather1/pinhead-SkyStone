723078a409fe73545f25797ea834524cd1dc7c57
carterjandrew <52466114+carterjandrew@users.noreply.github.com>
2020-01-01 16:54:19 -0700

    Added elevator max

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
@@ -1,6 +1,6 @@
 public class RobotVarables {
     public RobotVarables() {
-        int elevatorMax = 0; //<- elevator max height
+        int elevatorMax = 260; //<- elevator max height
         double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
         double trayServo[] = {0.3, 0.8}; //clasp down, clasp up
     }
