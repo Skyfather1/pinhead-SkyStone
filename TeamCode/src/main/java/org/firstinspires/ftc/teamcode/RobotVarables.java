8866103dbd55517b141d0219e6a75b3be9f3dabc
carterjandrew <52466114+carterjandrew@users.noreply.github.com>
2020-01-01 17:37:18 -0700

    Slight fix??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
@@ -1,8 +1,6 @@
 public class RobotVarables {
-    public RobotVarables() {
-        float slowMode = 0.3;
-        int elevatorMax = 260; //<- elevator max height
-        double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
-        double trayServo[] = {0.3, 0.8}; //clasp down, clasp up
-    }
+    public float slowMode = 0.3;
+    public int elevatorMax = 260; //<- elevator max height
+    public double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
+    public double trayServo[] = {0.3, 0.8}; //clasp down, clasp uppublic
 }
