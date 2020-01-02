69d0558f9fbad4021ea685cdabce1078392ec3ca
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-01-01 17:49:14 -0700

    Deleted slowmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
@@ -1,5 +1,4 @@
 public class RobotVarables {
-    public float slowMode = 0.3;
     public int elevatorMax = 260; //<- elevator max height
     public double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
     public double trayServo[] = {0.3, 0.8}; //clasp down, clasp uppublic
