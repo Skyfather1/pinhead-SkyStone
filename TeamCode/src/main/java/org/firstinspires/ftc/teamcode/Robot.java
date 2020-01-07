a5782dc73d36f64ce71b1622406ff2bdb6a18b5c
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-01-06 17:24:26 -0700

    Real Fix??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -82,7 +82,7 @@ public class Robot {
     }
     public boolean elevator(float upDown) {
         upDown = Range.clip(upDown, -1, 1);
-        if((elevatorSensor.getDistance(DistanceUnit.MM) <= 220 || upDown < 0) || (elevatorLimit.getState() == true)) {
+        if(!(elevatorSensor.getDistance(DistanceUnit.MM) <= 220 && upDown < 0) || !(elevatorLimit.getState() != true && upDown > 0)) {
             elevator.setPower(upDown);
             return true;
         }
