ee322eb9755bd894927fc06a848526388db36e1c
Someone <38514929+RealLifeWhale@users.noreply.github.com>
2020-01-12 18:43:21 -0700

    Staticed and finaled the varables idk if ill use these...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotVarables.java
@@ -1,5 +1,5 @@
 public class RobotVarables {
-    public int elevatorMax = 260; //<- elevator max height
-    public double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
-    public double trayServo[] = {0.3, 0.8}; //clasp down, clasp uppublic
+    public static final int elevatorMax = 260; //<- elevator max height
+    public static final double gripperServo[] = {0.4, 0.8}; //gripper open, gripper closed
+    public static final double trayServo[] = {0.3, 0.8}; //clasp down, clasp uppublic
 }
